d93696471dc7f2ddc9e056e1c4b401da2e442cf6
Tarun Sharma <ts5153033@gmail.com>
2019-06-12 05:07:12 -0400

    working with '>'

--- 11june/problem6.py
+++ 11june/problem6.py
@@ -1,8 +1,13 @@
-#!/bin/usr/python3
+#!/bin/python3
 import sys
 
 for i in sys.argv:
-        if i!=sys.argv[0]:
-            f=open(i,'r+')
-            print(f.strip())
-	    f.close()
+	if i!=sys.argv[0]:
+		if i[0] == '>' :
+			f=open(i[1:len(i)],'w')
+			f.write(input("Enter content:\n"))
+			f.close()
+		else :
+			f=open(i,'r+')
+			print(f.read())
+			f.close()

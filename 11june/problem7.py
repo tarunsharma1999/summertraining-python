43079ea10da6505db434f51e1e9a466fdc5652b9
Tarun Sharma <ts5153033@gmail.com>
2019-06-12 05:24:17 -0400

    '>' working now

--- 11june/problem7.py
+++ 11june/problem7.py
@@ -1,6 +1,12 @@
+#!/bin/python3
 import sys
 
 for i in sys.argv:
-        if i!=sys.argv[0]:
-            f=open(i,'a+')
-            f.close()
+	if i!=sys.argv[0]:
+		if i[0] == '>':
+			f=open(i[1:len(i)],'w')
+			f.write(input("Enter content of file:\n"))
+			f.close()
+		else :
+			f=open(i,'a+')
+			f.close()

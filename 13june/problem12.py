7dfa5cb46f38a05d77e5efbac10bcfeeac3c6ba1
root <root@ip-172-31-11-182.ap-south-1.compute.internal>
2019-06-15 11:06:18 +0000

    problem12 done

--- 13june/problem12.py
+++ 13june/problem12.py
@@ -9,5 +9,5 @@ for i in search(Tosearch,tld='com',lang='en',num=2,start=0,stop=4,pause=0):
 	li.append(i)
 for i in range(len(li)):
 	url=pyqrcode.create(li[i])
-	url.svg("search{}.png".format(i+1),scale=16)
+	url.svg("/var/www/html/search{}.svg".format(i+1),scale=16)
 
